9bc76fd1d23368693dd2839270502c3f150f90a0
KSVoss <Kai.S.Voss@t-online.de>
2022-07-14 15:38:10 +0200

    Initial Commit

---
+++ src/main/java/com/example/rum/RuMApplication.java
@@ -0,0 +1,13 @@
+package com.example.rum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuMApplication.class, args);
+	}
+
+}
